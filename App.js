1856dc060c797b3ab3da2a1157405eae1823f8ac
Harry MacCode <emarh.harry.code@gmail.com>
2024-01-12 23:58:36 +0100

    Initialization of our first React Native App

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Welcome from './components/Welcome';
 
 export default function App() {
+  const names = ['Matthieu', 'Marc', 'Luc', 'Jean'];
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app test!</Text>
+      <Text>Bonjour à tous !</Text>
       <StatusBar style="auto" />
+      {names.map((name, index) => {
+        return <Welcome key={index} name={name} />;
+      })}
+      
     </View>
   );
 }
